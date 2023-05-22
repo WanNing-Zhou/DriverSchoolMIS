0584790af4ad1b639c988646d4e9295ca100fafe
WangNing-Zhou <1879154660@qq.com>
2023-05-22 12:21:31 +0800

    阶段五: 学员样式

--- src/main/java/com/example/ssm/util/Permission.java
+++ src/main/java/com/example/ssm/util/Permission.java
@@ -4,7 +4,7 @@ package com.example.ssm.util;
  * @author 周万宁
  * @className Permission
  * @create 2023/5/17-12:14
- * @description
+ * @description 添加到接口上,表示该接口为需要认证的接口
  */
 import java.lang.annotation.*;
 
